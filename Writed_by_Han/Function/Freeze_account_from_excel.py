a776c7eaade1f4ce2208f418dd7f388e5965615a
user01_codecommit-at-273658691124 <han.duong.2@fecredit.com.vn>
2023-03-03 11:17:11 +0700

    upload function "freeze_excel_list_account"

--- Writed_by_Han/Function/Freeze_account_from_excel.py
+++ Writed_by_Han/Function/Freeze_account_from_excel.py
@@ -12,6 +12,68 @@ def lambda_handler(event, context):
     response = freeze_excel_list_account(event)
     return response
 
+# SU DUNG CAI NAY
+def invoke_freeze_excel_list_account(req_obj):
+    list_casa = req_obj.get('listCasa', [])
+    if len(list_casa):
+        # INVOKE LAMBDA FREEZE LIST ACCOUNT
+        try:
+            client = boto3.client(
+                'lambda',
+                aws_access_key_id='',
+                aws_secret_access_key=''
+            )
+            lambda_arn = 'arn:aws:lambda:ap-southeast-1:273658691124:function:sop-SupportFunction-gwrkNSzcOhad'
+            invoke_type = 'RequestResponse'
+            input_data = {
+                "env_id": "uat",
+                "func_name": "freeze_list_casa",
+                "data": {
+                    "reasonCode": "",
+                    "listCasa": list_casa
+                }
+            }
+            response = client.invoke (
+                FunctionName=lambda_arn,
+                InvocationType=invoke_type,
+                Payload=json.dumps(input_data)
+            )
+            response_from_func = json.load(response['Payload'])
+            print('RESPONSE FROM INVOKE LAMBDA----------->', response_from_func)
+            response_status = response_from_func.get('status', '')
+            if response_status == True:
+                return {
+                    'statusCode': 200,
+                    'status': True,
+                    'message': 'SUCCESS Freeze list account !',
+                    'listFreeze': list_casa
+                }
+            else:
+                return {
+                    'statusCode': 400,
+                    'status': False,
+                    'message': 'FAIL Freeze list account !',
+                    'listFreeze': list_casa
+                }
+        except Exception as e:
+            return {
+                'statusCode': 500,
+                'status': False,
+                'message': 'Internal server error'
+            }
+    return {
+        'statusCode': 400,
+        'status': False,
+        'message': 'MISSING FIELD !'
+    }
+
+
+
+
+
+
+
+
 
 # Task: Read file Excel --> get List CASA --> Invoke lambda function A Bao to freeze list account
 def freeze_excel_list_account(req_obj):
